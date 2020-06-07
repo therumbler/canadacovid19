430cf5b38ec4f705b7eee609166ea630a15caff2
Benjamin Rumble <therumbler@gmail.com>
2020-06-07 13:19:23 -0400

    add canada.py

---
+++ canadacovid19/canada.py
@@ -0,0 +1,21 @@
+"""https://www150.statcan.gc.ca/n1/tbl/csv/13100781-eng.zip
+"""
+
+
+from requests_html import AsyncHTMLSession
+from requests import Response
+
+
+async def _download_file():
+    session = AsyncHTMLSession()
+    url = "https://www150.statcan.gc.ca/n1/tbl/csv/13100781-eng.zip"
+    resp: Response = await session.get(session, stream=True)
+    filename = "tmp.zip"
+    with open(filename, "wb") as f:
+        for chunk in resp.iter_content(1024):
+            f.write(chunk)
+
+
+async def get_data():
+    await _download_file()
+    return {}
