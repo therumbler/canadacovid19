9949ff6030409c0119c2b9be99deba7865b16e32
Benjamin Rumble <therumbler@gmail.com>
2020-06-07 13:28:09 -0400

    csv reader

--- canadacovid19/canada.py
+++ canadacovid19/canada.py
@@ -1,6 +1,7 @@
 """https://www150.statcan.gc.ca/n1/tbl/csv/13100781-eng.zip
 """
-
+import csv
+from zipfile import ZipFile
 
 from requests_html import AsyncHTMLSession
 from requests import Response
@@ -14,6 +15,13 @@ async def _download_file():
     with open(filename, "wb") as f:
         for chunk in resp.iter_content(1024):
             f.write(chunk)
+
+    with ZipFile(filename) as zipfile:
+        with zipfile.open("13100781-eng/13100781.csv") as csvdata:
+            datareader = csv.reader(csvdata)
+            for row in datareader:
+                logger.info(", ".join(row))
+                break
 
 
 async def get_data():
