0895e5897dd2df4627d01b48377067c499d9b043
Static Files <staticfiles@bard.dreamhost.com>
2020-06-22 10:20:08 -0700

    added env var for PUBLIC_ALL_DATA_PATH

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 import asyncio
 import json
 import logging
+import os
 import sys
 from canadacovid19 import quebec, ontario, canada
 
 
 logger = logging.getLogger(__name__)
 
+PUBLIC_ALL_DATA_PATH = os.environ['PUBLIC_ALL_DATA_PATH']
 
 def setup_logging(level):
     logging.basicConfig(stream=sys.stderr, level=level)
@@ -37,7 +39,11 @@ async def main():
     with open("public/alldata.json", "w") as f:
         f.write(json.dumps(data))
 
+    with open(f"{PUBLIC_ALL_DATA_PATH}/alldata.json", "w") as f:
+        f.write(json.dumps(data))
+
+
 
 if __name__ == "__main__":
-    setup_logging(level=logging.DEBUG)
+    setup_logging(level=logging.INFO)
     asyncio.run(main())
